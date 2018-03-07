a8eb7da9ac73c3ba8959ad6472d207503843f27c
Jackie <jackie@ghtinc.com>
2018-03-07 21:20:03 +0800

    cmd.sh add tmux feature

--- python3/cmd.sh
+++ python3/cmd.sh
@@ -2,8 +2,10 @@
 
 if [ "$1" = "koans" ]; then
 	python3 contemplate_koans.py
+elif [ "$1" = "tmux" ]; then
+	tmux attach -t 0
 elif [ "$1" = "" ]; then
-	echo "./cmd.sh [koans|\$FILENAME]"
+	echo "./cmd.sh [koans|\$FILENAME|tmux]"
 else
 	vim ${1}
 fi
